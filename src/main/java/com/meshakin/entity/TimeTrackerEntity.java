0ac842cc6bb819aaa4987d242013a627873ab37d
Мешакин Максим Александрович <mesakinmaksim@gmail.com>
2025-09-25 02:11:02 +0300

    First

--- src/main/java/com/meshakin/entity/TimeTrackerEntity.java
+++ src/main/java/com/meshakin/entity/TimeTrackerEntity.java
@@ -4,7 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class TimeTrackerEntity {
 
