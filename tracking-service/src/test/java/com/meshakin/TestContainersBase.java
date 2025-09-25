4da2b8f906cecd536d8e3fba7bd766ee1d892fa6
Мешакин Максим Александрович <mesakinmaksim@gmail.com>
2025-09-26 02:42:50 +0300

    add basic tests

---
+++ tracking-service/src/test/java/com/meshakin/TestContainersBase.java
@@ -0,0 +1,18 @@
+package com.meshakin;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public class TestContainersBase {
+    protected static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17")
+            .withDatabaseName("testdb")
+            .withUsername("testuser")
+            .withPassword("testpass");
+
+    static {
+        postgres.start();
+        System.setProperty("spring.datasource.url", postgres.getJdbcUrl());
+        System.setProperty("spring.datasource.username", postgres.getUsername());
+        System.setProperty("spring.datasource.password", postgres.getPassword());
+        System.setProperty("spring.jpa.properties.hibernate.default_schema", "public");
+    }
+}
